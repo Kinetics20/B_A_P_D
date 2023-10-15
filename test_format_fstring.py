78ffcbfc033beb73a824ef0418e1c1221db4fe8b
kinetics20 <kinetics20@gmail.com>
2023-10-15 21:35:05 +0200

    Passing variables to strings

---
+++ test_format_fstring.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+cat = "John"
+result = "{} has a cat named {}".format("Agnes",cat)
+print(result)
+print("SECOND WAY fstring")
+result_1 =f"Agnes has a cat named {cat}"
+print(result_1)
+woman = "Jenny"
+man = "Kris"
+kid = "Jenny"
+result_2 = f"{woman} and {man} are parrents and they have kid named {kid} , it's their first child"
+print(result_2)
+print(f"We are learning Python {13//4}")
+print("sth is sth {},".format("where")*2)
