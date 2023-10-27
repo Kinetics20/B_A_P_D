b6ade277f02705418cf5ceb6159eafa41d1dca38
Kinetics20 <kinetics20@gmail.com>
2023-10-27 08:02:21 -0700

    lots_of_testing

---
+++ 12_.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+a = "Where's my coat"
+print(f"{a*10}")
---
+++ 13_.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+while True:
+    num = int(input('Podaj liczbe wieksza od 0: '))
+    if num > 0:
+        print('Twoja liczba to: ' , num)
+        break
+    else:
+        print('To nie jest prawidlowa liczba')
