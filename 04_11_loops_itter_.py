3ee1836f39f5df1b4373b05994239877386b8678
Kinetics20 <kinetics20@gmail.com>
2023-11-03 09:57:56 -0700

    loops_prework

---
+++ 04_11_loops_itter_.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+heroes = ["Dziewczynka_z_zapalkami",
+          "Mala_syrenka",
+          "Kubus_puchatek",
+          "Kopciuszek",
+          "Czerwony_Kapturek"]
+for character in heroes:
+    print(character)
+i=0
+while i<len(heroes):
+    print(heroes[i])
+    i += 1
