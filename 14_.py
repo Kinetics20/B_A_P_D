4ae768af8011f8c2345241e858c92e1c0afcabb7
Kinetics20 <kinetics20@gmail.com>
2023-11-03 20:59:08 +0100

    testing

--- 14_.py
+++ 14_.py
@@ -1,5 +1,40 @@
 #!/usr/bin/python3
-n = "Where is my home"
+# n = "Where is my home"
+#
+# for i in n:
+#     print(f"letters_from_string : {i}")
+#
+# k = 0
+# while k < len(n):
+#     list(n)
+#     print(n[k])
+#     k += 1
 
-for i in n:
-    print(f"letters_from_string : {i}")
+# a = input("Input sth : ")
+# print(f"Entered number is : {a}")
+#
+# b = 100 / 33
+# print(f"integer of b is : {int(b)}")
+
+# from random import randint
+#
+#
+# rnd = randint(10, 20)
+#
+# my_str = "What you're gonna do with it ?"
+# my_list = my_str.split()
+# # d = list(my_str)
+# print(my_list)
+# my_str_2 = " : ".join(my_list)
+# print(f"Elements od string : {my_str_2}")
+# print(my_str.upper())
+# print(my_str.lower())
+
+# from random import randint
+#
+# rnd = randint(10, 230)
+
+a = range(0, 10)
+b = list(a)
+print(b, end=" ")
+
