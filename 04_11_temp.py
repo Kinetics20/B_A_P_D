0e174d2e202eeec1b3bdef9f849ac32c16ba1ece
Kinetics20 <kinetics20@gmail.com>
2023-11-03 09:44:43 -0700

    task_02_04_11_

---
+++ 04_11_temp.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+temp = 15
+temp_2 = "15"
+mult = temp * 5
+mult_1 = temp_2 * 10
+mult_2 = temp / 5
+print(f"multiple int : {mult,type(mult)}, multiple str : {mult_1,type(mult_1)} , score of dividing is : {mult_2,type(mult_2)}")
