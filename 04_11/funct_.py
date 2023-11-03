d5428940f70d247e7adc7a7a603e9715c954dd9b
Kinetics20 <kinetics20@gmail.com>
2023-11-03 22:27:42 +0100

    function

---
+++ 04_11/funct_.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def num_sum(x, y, z=30):
+    g_sum = x + y + z
+    return g_sum
+
+
+num_1 = 400
+num_2 = 500
+num_sum_1 = num_sum(num_1, num_2)
+print(f"sum of {num_1} and {num_2} is {num_sum_1} ")
