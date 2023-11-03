6339b4aa3c9cb38643573f488b26915595f85d16
Kinetics20 <kinetics20@gmail.com>
2023-11-03 07:12:05 -0700

    corrected_file

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 my_list = [1, 2, 3, 'World', 'Home', 'Toy_car', 3.6]
-print('things from my_list: {}'.format(my_list, type(my_list)))
-
+# my_list = [100550034324324, 2324324, 132141533, 'World', 'Home', 'Toy_car', 3.6]
 for n in my_list:
-    if type(my_list[n]) is not str:
-        print(f"value of my_list is integer : {my_list[n]}")
-    if type(my_list[n]) is str:
-        print(f"value of my_list is sting :  {my_list[n]}")
-    else:
-        print(f"value of my_list is float :  {my_list[n]}")
+    if type(n) is str:
+        print(f"value of my_list is string : {n}")
+    if type(n) is float:
+        print(f"value of my_list is float :  {n}")
+    if type(n) is int:
+        print(f"value of my_list is integer :  {n}")
