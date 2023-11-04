38beabb05d7a4b1a0d7a30f7d2fa621302d03caf
Kinetics20 <kinetics20@gmail.com>
2023-11-04 08:40:01 +0100

    sth

--- 04_11/funct_.py
+++ 04_11/funct_.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-def num_sum(x, y, z=30):
-    g_sum = x + y + z
-    return g_sum
+# def num_sum(x, y, z=30):
+#     g_sum = x + y + z
+#     return g_sum
+#
+#
+# num_1 = 400
+# num_2 = 500
+# num_sum_1 = num_sum(num_1, num_2)
+# print(f"sum of {num_1} and {num_2} is {num_sum_1} ")
+
+def circle_circuit(diameter):
+    pi = 3.14
+    circuit = diameter / 2 * pi
+    return circuit
 
 
-num_1 = 400
-num_2 = 500
-num_sum_1 = num_sum(num_1, num_2)
-print(f"sum of {num_1} and {num_2} is {num_sum_1} ")
+diameter = float(input("Enter a diameter : "))
+new_circle_circuit = circle_circuit(diameter)
+print(f"circle circuit for diameter {diameter} is {new_circle_circuit}")
