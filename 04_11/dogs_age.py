106f3c9a5f3163caa6b122e205b2bfcea3f3cf50
Kinetics20 <kinetics20@gmail.com>
2023-11-14 00:12:55 +0100

    testing_function

--- 04_11/dogs_age.py
+++ 04_11/dogs_age.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
+from random import randint
+
+
 def dogs_age_1(age):
     dogs_age_y = age * 10.5
     return dogs_age_y
@@ -17,3 +20,14 @@ if num_1 <= 2:
 if num_1 > 2:
     age_of_dog = dogs_age_2(num_1)
     print(f"dog's age is : {age_of_dog}")
+
+
+def dogs_age_3():
+    age = randint(1, 14)
+    if age < 2:
+        return age * 10.5
+    else:
+        return 21 + (age - 2) * 4
+
+
+print(dogs_age_3())
