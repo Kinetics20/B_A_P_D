e338223fde50bb2eb559a9548e9fb697a3cf9e78
Kinetics20 <kinetics20@gmail.com>
2023-11-24 09:01:03 -0800

    Testing

--- workshop/try_exam/task_02.py
+++ workshop/try_exam/task_02.py
@@ -1,6 +1,6 @@
 def name_sorter(any_list):
     new_dict = {'female': [], 'male': []}
-    for word in any_list:
+    for word in sorted(any_list):
         if word[-1] == 'a':
             new_dict['female'].append(word)
         else:
