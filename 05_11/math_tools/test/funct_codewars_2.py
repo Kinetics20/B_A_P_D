f32b8eb2f93b7271676f230f55844fbf67c4920d
Kinetics20 <kinetics20@gmail.com>
2024-02-09 00:01:29 +0100

    testing

--- 05_11/math_tools/test/funct_codewars_2.py
+++ 05_11/math_tools/test/funct_codewars_2.py
@@ -879,3 +879,12 @@ def count_smileys(arr):
 
 def duck_duck_goose(players, goose):
     return players[(goose - 1) % len(players)].name
+
+
+def same_case(a, b):
+    if not a.isalpha() or not b.isalpha():
+        return -1
+    elif a.islower() == b.islower() or a.isupper() == b.isupper():
+        return 1
+    else:
+        return 0
