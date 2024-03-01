862e0ab9d789d910d43cd9b73994eb85b237e4ca
Kinetics20 <kinetics20@gmail.com>
2024-03-01 06:53:03 -0800

    testing

--- 05_11/math_tools/test/funct_codewars_2.py
+++ 05_11/math_tools/test/funct_codewars_2.py
@@ -1781,3 +1781,22 @@ def accum(s):
 
 def find(array, el):
     return array.index(el) if el in array else 'Not found'
+
+
+def decrypt(text, n):
+    if text in ("", None):
+        return text
+
+    ndx = len(text) // 2
+
+    for i in range(n):
+        a = text[:ndx]
+        b = text[ndx:]
+        text = "".join(b[i:i + 1] + a[i:i + 1] for i in range(ndx + 1))
+    return text
+
+
+def encrypt(text, n):
+    for i in range(n):
+        text = text[1::2] + text[::2]
+    return text
