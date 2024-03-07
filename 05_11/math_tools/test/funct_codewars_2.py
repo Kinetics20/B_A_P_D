5e82bc1b0df6c4649ad4398cd729fe899b5844de
Kinetics20 <kinetics20@gmail.com>
2024-03-07 08:46:36 -0800

    testing

--- 05_11/math_tools/test/funct_codewars_2.py
+++ 05_11/math_tools/test/funct_codewars_2.py
@@ -2040,3 +2040,7 @@ def sale_hotdogs_3(n):
 class Ball:
     def __init__(self, ball_type="regular"):
         self.ball_type = ball_type
+
+
+def zero_fuel(distance_to_pump, mpg, fuel_left):
+    return mpg * fuel_left >= distance_to_pump
