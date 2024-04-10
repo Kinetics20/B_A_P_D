9966b27039774ff71a4ae100f4912349fa2f6071
Kinetics20 <kinetics20@gmail.com>
2024-04-10 05:13:20 -0700

    testing

--- 05_11/math_tools/test/funct_codewars_2.py
+++ 05_11/math_tools/test/funct_codewars_2.py
@@ -2985,3 +2985,33 @@ def remove_url_anchor_2(url):
 
 def remove_url_anchor_3(url):
     return url.partition('#')[0]
+
+
+class Fighter(object):
+    def __init__(self, name, health, damage_per_attack):
+        self.name = name
+        self.health = health
+        self.damage_per_attack = damage_per_attack
+
+    def __str__(self): return "Fighter({}, {}, {})".format(self.name, self.health, self.damage_per_attack)
+
+    __repr__ = __str__
+
+
+def declare_winner(fighter1, fighter2, first_attacker):
+    current_attacker = fighter1 if fighter1.name == first_attacker else fighter2
+    current_defender = fighter2 if fighter1.name == first_attacker else fighter1
+
+    while True:
+        current_defender.health -= current_attacker.damage_per_attack
+        if current_defender.health <= 0:
+            return current_attacker.name
+        current_attacker, current_defender = current_defender, current_attacker
+
+
+def declare_winner_1(fighter1, fighter2, first_attacker):
+    cur, opp = (fighter1, fighter2) if first_attacker == fighter1.name else (fighter2, fighter1)
+    while cur.health > 0:
+        opp.health -= cur.damage_per_attack
+        cur, opp = opp, cur
+    return opp.name
