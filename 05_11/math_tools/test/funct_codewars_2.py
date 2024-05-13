8f9be92497d1fee268c59fb8a065d3b8a7531eef
Kinetics20 <kinetics20@gmail.com>
2024-05-13 06:36:19 -0700

    testing

--- 05_11/math_tools/test/funct_codewars_2.py
+++ 05_11/math_tools/test/funct_codewars_2.py
@@ -3128,3 +3128,11 @@ def diamond(n):
         diamond_str += " " * spaces + "*" * diamonds + "\n"
 
     return diamond_str
+
+def divisors(n):
+    divisors_count = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            divisors_count += 1
+    return divisors_count
+
