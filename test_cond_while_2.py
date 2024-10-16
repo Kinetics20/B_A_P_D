a53e47e8e17b562d34f0d5bf93ae4e4b4f8ff84d
unknown <D:\programs\The Bat!\Mail>
2024-10-16 20:32:22 +0200

    testing_connection

--- test_cond_while_2.py
+++ test_cond_while_2.py
@@ -7,7 +7,8 @@ while count < 30:
     count += 1
     if count == 14:
         break
-    if count %2 == 0:
+    # if count %2 == 0:
+    if count & 1 == 0:
         continue
     print("To sa liczby nieparzyste : {}".format(count))
     # if count %2 != 0:
